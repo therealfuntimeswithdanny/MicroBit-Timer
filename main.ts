914c152b169ae3826f438e0fc23460fa357a7853
Daniel Morrisey <dannymorrisey1@outlook.com>
2025-06-14 14:29:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.AB, function () {
+    while (time > 0) {
+        basic.pause(1000)
+        time += -1
+        basic.showNumber(time)
+    }
+    music.play(music.stringPlayable("C C5 C C5 C C5 C C5 ", 120), music.PlaybackMode.UntilDone)
+})
+let time = 0
+time = 5
+basic.showNumber(time)
